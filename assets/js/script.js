97b5189a2b75357b45c38584adfe762a3d74fcb8
Samuel Näsström <nasstromsamuel@icloud.com>
2022-09-18 14:14:43 +0000

    Add coded to calculate two random numbers for the game quest question

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,49 +2,55 @@
 // Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function () {
-            let buttons = document.getElementsByTagName('button');
+    let buttons = document.getElementsByTagName('button');
 
-            for (let button of buttons) {
-                button.addEventListener("click", function () {
-                        if (this.getAttribute("data-type") === "submit") {
-                            alert("You clicked Submit!")
-                        } else {
-                            let gameType = this.getAttribute("data-type");
-                            alert(`You clicked ${gameType}`)
-                        }
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`)
+            }
 
-                    })
-                }
-            })
+        })
+    }
+})
 
-        function runGame() {
 
-        }
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
 
-        function checkAnswer() {
+function checkAnswer() {
 
-        }
+}
 
-        function calculateCorrectAnswer() {
+function calculateCorrectAnswer() {
 
-        }
+}
 
-        function incrementScore() {
+function incrementScore() {
 
-        }
+}
 
-        function incrementWrongAnswer() {
+function incrementWrongAnswer() {
 
-        }
+}
 
-        function displayAdditionQuestion() {
+function displayAdditionQuestion() {
 
-        }
+}
 
-        function displaySubtractQuestion() {
+function displaySubtractQuestion() {
 
-        }
+}
 
-        function displayMultiplyQuestion() {
+function displayMultiplyQuestion() {
 
-        }
+}
